11fbf7f55276b9dbb35a5eb0587f4f52ca40aa33
chxmek <Chawanwits.mek@gmail.com>
2024-08-15 22:48:54 +0700

    add model annotation

--- BasicASPTutorial/Models/Student.cs
+++ BasicASPTutorial/Models/Student.cs
@@ -1,8 +1,12 @@
-﻿namespace BasicASPTutorial.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicASPTutorial.Models
 {
-    public class Student
+    public class Student                   // ใน sql ชื่อ model จะเป็นชื่อตาราง
     {
-        public int Id { get; set; }
+        [Key]                              // ระบุ data annotation ชื่อว่า Key คือต้องการให้ props Id เป็นรูปแบบ primary key >> สร้างรหัส(Id)ให้อัตโนมัติ
+        public int Id { get; set; }        // ชื่อ property จะเป็นชื่อ column ในตารางนั้นๆ
+        [Required]                         // Name ห้ามเป็นค่าว่าง
         public string Name { get; set; }
         public int Score { get; set; }
     }
